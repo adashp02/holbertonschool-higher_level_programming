33aa1cabaef5eb1031063aafe542fb6d3dce1029
adashp02 <adashp02@gmail.com>
2025-05-07 17:02:59 +1000

    print lowercase alphabet

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    # chr converts asciii number into char
+# chr converts asciii number into char
+# end makes all in one line w/wo spaces
     print("{}".format(chr(i)), end="")
