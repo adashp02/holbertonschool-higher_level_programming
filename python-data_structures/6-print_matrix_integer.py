44f1b2afaa909ba7a300f556ebfa1d2948631b1a
adashp02 <adashp02@gmail.com>
2025-05-14 21:33:13 +1000

    print matrix in rows of 3

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print("{:d} {:d} {:d}".format(row[0], row[1], row[2]))
+        non_row = row + [''] * (3 - len(row))
+        print("{:d} {:d} {:d}".format(non_row[0], non_row[1], non_row[2]))
+    if not matrix:
+        print()
