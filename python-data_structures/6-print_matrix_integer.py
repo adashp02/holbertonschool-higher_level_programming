b2393adbc4bfcdce6edd25f9bc9586e929f196b9
adashp02 <adashp02@gmail.com>
2025-05-14 21:21:31 +1000

    print matrix in rows of 3

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        #{:3} formats each number to be right-aligned within 3 spaces
-        print("{:3} {:3} {:3} |".format(row[0], row[1], row[2]))
+        print("{:d} {:d} {:d} |".format(row[0], row[1], row[2]))
