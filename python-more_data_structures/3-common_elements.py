af38cacb228e452b1b89a1b5bde5f22980ce0672
adashp02 <adashp02@gmail.com>
2025-05-19 16:26:58 +1000

    returns unique elements from two sets

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     """Return a set of common elements in two sets."""
+    """operator '&' displayes common elements in sets"""
     return (set_1 & set_2)
