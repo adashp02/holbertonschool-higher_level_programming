e0a158595fe53f730e680aabd5ce56aaddddc1e3
adashp02 <adashp02@gmail.com>
2025-05-21 20:43:22 +1000

    create class Square, initialize an instance  with a private attribute size

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+"""
+This module creates class Square 
+with private attribute 'size'
+"""
+
+class Square:
+
+    """initialize the instance of Square class
+    with attribute size"""
+
+    def __init__(self, size):
+
+        """size is a private attribute"""
+
+        self.__size = size
