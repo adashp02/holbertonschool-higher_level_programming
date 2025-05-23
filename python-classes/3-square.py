d4d98e09f8fa9e4789a7b022f28b46414e952e90
adashp02 <adashp02@gmail.com>
2025-05-23 16:51:48 +1000

    return area of the instance square

---
+++ python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+"""
+This module creates class Square
+with private attribute 'size'
+"""
+
+
+class Square:
+
+    """initialize the instance of Square class
+    with attribute size"""
+
+    def __init__(self, size=0):
+
+        """if size is not an integer"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        """size is a private attribute"""
+
+        self.__size = size
+        def area(self):
+            area = size ** 2
