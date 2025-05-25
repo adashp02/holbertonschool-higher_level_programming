50da9efc6acddfe24926fd975abf01698b93cb8e
adashp02 <adashp02@gmail.com>
2025-05-25 14:18:20 +1000

    print square from instance

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -15,22 +15,22 @@ class Square:
     def __init__(self, size=0):
         self.size = size
         
-        """getter - get the size of the square"""
-        @property
-        def size(self):
+    """getter - get the size of the square"""
+    @property
+    def size(self):
             return (self.__size)
 
-        """setter - same name as attribute"""
-        @size.setter
-        def size(self, value):
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
+    """setter - same name as attribute"""
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
 
-            elif value < 0:
-                raise ValueError("size must be >= 0")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
             """size is a private attribute"""
             self.__size = value
 
-        def area(self):
-            """return the area of the suare"""
-            return (self.__size ** 2)
+    def area(self):
+        """return the area of the suare"""
+        return (self.__size ** 2)
