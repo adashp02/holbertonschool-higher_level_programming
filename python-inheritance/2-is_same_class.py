5df89bf25f03b45dc6d1fbe64bf5625365243c2e
adashp02 <adashp02@gmail.com>
2025-05-26 18:15:21 +1000

    is the object an instance of a specified class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""function that returns True if
+an object is an instance of a specified class"""
+
+
+def is_same_class(obj, a_class):
+    """function validating that obj is an instance
+    of a_class
+
+    Args:
+        obj - object to be evaluated
+        a_class - class to be compared against
+    
+    Returns:
+        False or True
+    """
+    return type(obj) == a_class
