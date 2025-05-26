3c011e2df71ed7861bf5669542ecc4140111743d
adashp02 <adashp02@gmail.com>
2025-05-26 17:17:54 +1000

    list all available attributes and methods of an object

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""this module creates lookup function
+that lists attr and methods of an object"""
+
+def lookup(obj):
+
+    """it returns a list of attr and methods 
+    using built-in function dir"""
+
+    return dir(obj)
