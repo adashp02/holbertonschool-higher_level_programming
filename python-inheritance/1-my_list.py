3d034b7294b1d945e1ee2900b748d4380395fd34
adashp02 <adashp02@gmail.com>
2025-05-26 17:30:09 +1000

    class MyList with a function printing a sorted list of integers

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""creates a class MyList with inheritance
+from list and prints contents of a list of ints sorted"""
+
+
+class MyList(list):
+
+    def print_sorted(self):
+        """this function will print a sorted list of ints"""
+
+        print(sorted(self))
