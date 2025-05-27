49261115f69a6f85c7195b3346a9c036d083f1f2
adashp02 <adashp02@gmail.com>
2025-05-27 10:16:25 +1000

    checks if instance of an inherited from a subclass, not base class

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""function that returns True if
+an object is an instance of a class inherited
+from a specified class
+"""
+
+
+def inherits_from(obj, a_class):
+    """function validating that obj is an instance
+    of a_class inherited from a specified class
+
+    Args:
+        obj - object to be evaluated
+        a_class - class to be checked against
+
+    Returns:
+        False or True
+    """
+    if issubclass(type(obj), a_class) and not type(obj) == a_class:
+        return True
+    return False
