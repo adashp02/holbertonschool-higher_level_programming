efeebb3c566c0507d4e6e6898620faed318b1ce6
adashp02 <adashp02@gmail.com>
2025-05-27 13:46:15 +1000

    validates the value within the method

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -9,4 +9,5 @@ This module creates a new class BaseGeometry
 class BaseGeometry:
     """ public instance method """
     def area(self):
+        """area not yet implemented"""
         raise Exception("area() is not implemented")
---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+
+"""
+This module creates a new class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ public instance method """
+    def area(self):
+        """area not yet implemented"""
+        raise Exception("area() is not implemented")
+
+    """public unstance method"""
+    def integer_validator(self, name, value):
+        """
+        validates value as int
+
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
+
+        if type(value) is not int:
+            raise TypeError ("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError ("{} must be greater then 0".format(name))
