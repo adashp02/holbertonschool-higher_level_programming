883c641973996cbee3e6d4d953d3f77d35a183e1
adashp02 <adashp02@gmail.com>
2025-05-27 13:19:36 +1000

    exception:area is not implemented

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+"""
+This module creates a new class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ public instance method """
+    def area(self):
+        raise Exception("area() is not implemented")
+
