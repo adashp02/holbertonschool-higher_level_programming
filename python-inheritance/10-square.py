fdd0f388a47b271732af67278f6956397e7e5bc3
adashp02 <adashp02@gmail.com>
2025-05-27 22:22:44 +1000

    create subclass Square inherited from Rectangle, with private attr size that is a positive int - validated

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """importing class Rectangle from another file"""
-Rectangle = __import__('9-rectangle.py').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
