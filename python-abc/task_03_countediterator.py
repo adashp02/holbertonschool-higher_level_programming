24600b242d491ef00cb8dab7d27d8f975ecc96d7
adashp02 <adashp02@gmail.com>
2025-05-30 19:59:38 +1000

    Create a class named CountedIterator that extends the built-in iterator obtained from the iter function. The CountedIterator should keep track of the number of items that have been iterated over. Specifically, you will need to override the __next__ method to increment a counter each time an item is fetched.

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+class CountedIterator:
+    """iterator that keeps truck of the number
+    of items iterated over"""
+
+    def __init__(self, iterable):
+        """Initialize the iterator and the counter"""
+
+        self.iterator = iter(iterable)
+        self.count = 0
+
+    def __next__(self):
+        """return the next item and increment counter"""
+
+        try:
+            item = next(self.iterator)
+            self.count += 1
+            return item
+        except StopIteration:
+            raise StopIteration
+        
+    def get_count(self):
+        """Return the current count of items iterated over"""
+
+        return self.count
+    
+    def __iter__(self):
+        """Return the iterator itself"""
+
+        return self
+
