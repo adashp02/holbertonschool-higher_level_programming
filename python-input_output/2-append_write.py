7f535525712fdae06baced670afaa0fe96468a38
adashp02 <adashp02@gmail.com>
2025-06-08 13:38:47 +1000

    function appends a string to a file and returns the number of chars

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -12,5 +12,5 @@ def append_write(filename="", text=""):
     Returns:
         The number of characters appended."""
     with open(filename, 'a', encoding='utf-8') as f:
-        f.append(text)
+        f.write(text)
         return len(text)
