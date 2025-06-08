2b37396b8685b1d601f2263bc63cb0b68da93992
adashp02 <adashp02@gmail.com>
2025-06-08 11:43:57 +1000

    function that reads a text file (UTF8) and prints it to stdout

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,6 +2,7 @@
 """function that reads a text file (UTF8)
 and prints it to stdout"""
 
+
 def read_file(filename=""):
     with open(filename, encoding="UTF=8") as f:
         """Print the contents of a UTF8 text file to stdout."""
