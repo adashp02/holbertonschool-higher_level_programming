b51424e424353a2d5305f6f52e14238efc50e87d
adashp02 <adashp02@gmail.com>
2025-06-08 12:00:06 +1000

    function that writes a string to a text file (UTF8) and returns the number of characters written

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """function that writes a string to a text file (UTF8)
-and returns the number of characters written:
+and returns the number of characters written
 """
+
 
 def write_file(filename="", text=""):
     """Write a string to a UTF8 text file.
