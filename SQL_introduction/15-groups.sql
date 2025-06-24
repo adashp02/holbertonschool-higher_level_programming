ed8a911441b4241bdf4b2874e856e924de63018e
adashp02 <adashp02@gmail.com>
2025-06-24 17:21:02 +1000

    display groups of people with the same score, labeled with numbers in desc order

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- display groups of people with the same score, labeled with numbers in desc order
-SELECT score, COUNT(name) AS number FROM second_table GROUP BY score ORDER BY number;
+SELECT score, COUNT(name) AS number FROM second_table GROUP BY score ORDER BY number DESC;
