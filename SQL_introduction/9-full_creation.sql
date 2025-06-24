8968c8771ce1c9d12d4dd8df661c294942ee55b7
adashp02 <adashp02@gmail.com>
2025-06-24 16:12:03 +1000

    create another table with 3 columns and 4 rows

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,8 @@
+-- create a new table with 3 columns and 4 rows
+CREATE TABLE IF NOT EXISTS second_table (id int, name varchar(256), score int);
+INSERT INTO second_table (id, name, score) 
+VALUES 
+    ('1', 'John', '10'),
+    ('2', 'Alex', '3'),
+    ('3', 'Bob', '14'),
+    ('4', 'George', '8');
