4186d50c8aba3c41bba054e7798b03a48754a5cc
adashp02 <adashp02@gmail.com>
2025-06-29 15:56:43 +1000

    creates the database hbtn_0d_usa and the table cities, with column state_id, that is a foreign key referencing to id of the states

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- creates the database hbtn_0d_usa and the table cities, with column state_id
+-- that is a foreign key referencing to id of the states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+    USE hbtn_0d_usa;
+    CREATE TABLE IF NOT EXISTS cities (
+        id int NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+        state_id int NOT NULL FOREIGN KEY REFERENCES states(id),
+        name varchar(256) NOT NULL
+    );
