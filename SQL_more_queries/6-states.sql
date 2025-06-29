61d2aefdafdf4e79cf32a1c07350f1e952718cda
adashp02 <adashp02@gmail.com>
2025-06-29 15:44:31 +1000

    creates the database hbtn_0d_usa and the table states, id column is primary key and unique auto_generated

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- creates the database hbtn_0d_usa and the table states
     CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+    USE hbtn_0d_usa;
     CREATE TABLE IF NOT EXISTS states (
-        id int NOT NULL AUTO_INCREMENT UNIQUE,
+        id int NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
         NAME varchar(256) NOT NULL,
-        PRIMARY KEY (id)
     );
