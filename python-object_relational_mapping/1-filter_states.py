525345648121e10c5319f575e56b2745142b48c6
adashp02 <adashp02@gmail.com>
2025-07-01 18:26:16 +1000

    script that lists states with first letter N using module MySQLdb

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script that lists states with first letter N"""
+
+
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
+
+    db = MySQLdb.connect(
+        user=argv[1],
+        password=argv[2],
+        database=argv[3]
+    )
+    cursor = db.cursor()
+
+    cursor.execute('SELECT * FROM states ORDER BY id')
+
+    for state in cursor.fetchall():
+        if state[1][0] == 'N':
+            print(state)
+
+    cursor.close()
+    db.close()
